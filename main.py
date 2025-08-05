110597218fa2486dc2737c9748c7b30d8aef557a
George Hannah <80538951+Ge0rgeHannah@users.noreply.github.com>
2025-08-05 14:59:10 +0100

    Added schema loading

--- main.py
+++ main.py
@@ -1,6 +1,39 @@
 # Entry to RELRaE
+import argparse
+
+from schema_processor import Schema
+
+
+def run(args):
+    schema = args.schema
+
+    # Process schema
+    current_schema = Schema(schema)
+    current_schema.extract_concepts()
+    current_schema.fragment_generation()
+    fragments_list = current_schema.return_fragments()
+
+    # Refine fragments
+
+    # Create Graph
+
+
 def main():
-    pass
+    parser = argparse.ArgumentParser(
+        prog='RELRaE 2',
+        description='A LLM-enhanced framework for translating an XML schema into an RDFS ontology'
+    )
+    parser.add_argument(
+        '-s',
+        type=str,
+        help="The path of the input XML schema",
+        dest='schema',
+        required=True
+    )
+
+    parser.set_defaults(func=run)
+    args = parser.parse_args()
+    args.func(args)
 
 
 if __name__ == '__main__':
--- schema_processor.py
+++ schema_processor.py
@@ -6,3 +6,20 @@ class Schema:
 
     def __init__(self, path):
         self.path = path
+        self.schema, self.root, self.ns = self.load_xml()
+
+    def load_xml(self):
+        schema = ET.parse(self.path)
+        root = schema.getroot()
+        ns = {'xs': "http://www.w3.org/2001/XMLSchema",
+              'xsd': "http://www.w3.org/2001/XMLSchema"}
+        return schema, root, ns
+
+    def extract_concepts(self):
+        pass
+
+    def fragment_generation(self):
+        pass
+
+    def return_fragments(self):
+        pass
