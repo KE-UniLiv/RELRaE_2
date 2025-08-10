4bf060b402aaac55cdac24cbaa348b80ebd68504
George Hannah <80538951+Ge0rgeHannah@users.noreply.github.com>
2025-08-10 16:18:40 +0100

    Define element objects

--- schema_processor.py
+++ schema_processor.py
@@ -2,6 +2,51 @@
 from lxml import etree as ET
 
 
+# Define element classes
+# TODO: Add cardinality support
+
+class AttributeGroup():
+
+    def __init__(self, name, attributes):
+        self.name = name
+        self.attributes = attributes
+
+
+class ComplexTypeElement():
+
+    def __init__(self, name, description, children, attributes, definition):
+        self.name = name
+        self.description = description
+        self.children = children
+        self.attributes = attributes
+        self.definition = definition
+
+
+class SimpleTypeElement():
+
+    def __init__(self, name, description, restriction):
+        self.name = name
+        self.description = description
+        self.restriction = restriction
+
+
+class Attribute():
+
+    def __init__(self, name, description, type_, use):
+        self.name = name
+        self.description = description
+        self.type_ = type_
+        self.use = use
+
+
+class ExternallyDefinedElement():
+
+    def __init__(self, name, description, type_):
+        self.name = name
+        self.description = description
+        self.type_ = type_
+
+
 class Schema:
 
     def __init__(self, path):
